a60529ec1513e1aa7ee378351139b8d1a4103eee
unknown <staub.joao.cs@gmail.com>
2019-10-21 23:37:00 -0300

    Base de dados quase funcionando

--- BaseDados/basededados.php
+++ BaseDados/basededados.php
@@ -1,25 +1,10 @@
 <?php
-// for($i = 1; $i <= 4000; $i++){
-//     $n1 = rand(0, 9);
-//     $n2 = rand(0, 9);
-//     $n3 = rand(0, 9);
-//     $n4 = rand(0, 9);
-//     $n5 = rand(0, 9);
-//     $n6 = rand(0, 9);
-//     $n7 = rand(0, 9);
-//     $n8 = rand(0, 9);
-//     $n9 = rand(0, 9);
-//     $n10 = rand(0, 9);
-//     $n11 = rand(0, 9);
-
-//     $cpf = $n1.$n2.$n3.".".$n4.$n5.$n6.".".$n7.$n8.$n9."-".$n10.$n11;
-//     echo "'".$cpf."'"."AAAAAA";
-// }
-
 for($i = 1; $i <= 4000; $i++){
-    $lmin = 'abcdefghijklmnopqrstuvwxyz';
-    $lmai = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $n1 = rand(1, 3976);
 
-    echo $l1;
-}
+    $cidade = $n1;
+    echo "'".$cidade."'"."AAAAAA";
+} 
+
+// 87.600.000
 ?>
