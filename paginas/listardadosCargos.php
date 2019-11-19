4fea19d5e262ba48c7e8c9aa3f053a991a96f668
Aluno <staub.joao.cs@gmail.com>
2019-11-19 11:46:55 -0200

    Cofigurando o BD

--- paginas/listardadosCargos.php
+++ paginas/listardadosCargos.php
@@ -1,6 +1,3 @@
-<br>
-<br>
-<br>
 <?php
 $con = conecta();
 // print_r($_GET);
@@ -16,7 +13,18 @@ if($resCurriculo){
         <div class="col-md-8 ml-md-5  border border-primary  shadow-lg p-3 mb-5 bg-white rounded"
             style="margin-top: 100px">
             <div class="row">
-                <h2><?php echo$curriculos["nome"]; ?></h2>
+                <div class="col-sm-9">
+                    <h1><?php echo $curriculos["nome"]; ?></h1>
+                </div>
+                <div class="col-sm-3">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Morbi leo risus</li>
+                        <li class="list-group-item">Porta ac consectetur ac</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                </div>
             </div>
             </div>
         </div>
